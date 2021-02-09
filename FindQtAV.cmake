5d7f034bccd8df01b6f4213823e29f53065629d2
Allen Wild <allenwild93@gmail.com>
2021-02-09 00:27:03 -0500

    cmake cleanup
      * refactor FindQtAV after I finally found a good example of using
        components sanely. It's a lot more readable and straightforward now.
      * document outputs and cache variables in FindQtAV
      * set colored compiler output when using ninja - both gcc 9 and clang
        10 support -fdiagnostics-color=always
      * remove LGPL-3.0 license comment, it doesn't really make much sense
        for build files

--- FindQtAV.cmake
+++ FindQtAV.cmake
@@ -1,15 +1,24 @@
-# based on https://github.com/wang-bin/QtAV/blob/dfb3425db1c2658abb65c3ce795fdcde31f17052/cmake/FindQtAV.cmake
+# Try to find the QtAV library
+# Author: Allen Wild
 #
-# - Try to find the QtAV library
+# based on [1] and incorporating advice from [2]
 #
-# Standard find_package output variables are set, including QtAV_FOUND, QtAV_INCLUDE_DIRS, and
-# QtAV_LIBRARIES.  If the Widgets component is requested, also set QtAV_Widgets_FOUND,
-# QtAVWidgets_INCLUDE_DIRS, and QtAVWidgets_LIBRARIES
+# [1] https://github.com/wang-bin/QtAV/blob/dfb3425db1c2658abb65c3ce795fdcde31f17052/cmake/FindQtAV.cmake
+# [2] https://dev.to/slurpsmadrips/how-to-find-packages-with-cmake-the-basics-ikk
 #
-# Also create imported library targets QtAV::QtAV and QtAV::Widgets which can be used with
-# target_link_libraries.
+# OUTPUTS:
+# Imported library targets QtAV::QtAV and QtAV::QtAVWidgets that can be used with
+# target_link_library.  QtAV::QtAVWidgets is only available when requesting the Widgets
+# component with find_package, and depends on QtAV::QtAV.
 #
-# TODO: support user-defined cache variables
+# CACHE VARIABLES (advanced, for user-defined paths):
+#   QTAV_INCLUDE_DIR        - Include path for QtAV headers
+#   QTAV_LIBRARY            - Path to libQtAV library file
+#   QTAVWIDGETS_INCLUDE_DIR - Include path for QtAVWidgets headers
+#   QTAVWIDGETS_LIBRARY     - Path to libQtAVWidgets library file
+#
+# Users of this module should prefer the imported library targets, don't rely on the bare
+# include_dir and library variables.
 
 include(FindPackageMessage)
 include(FindPackageHandleStandardArgs)
@@ -19,67 +28,77 @@ find_package(Qt5 QUIET REQUIRED NO_MODULE COMPONENTS Core)
 get_target_property(qmake Qt5::qmake LOCATION)
 execute_process(
     COMMAND ${qmake} -query QT_INSTALL_HEADERS
-    OUTPUT_VARIABLE QT_INSTALL_HEADERS
+    OUTPUT_VARIABLE _QT_INSTALL_HEADERS
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 execute_process(
     COMMAND ${qmake} -query QT_INSTALL_LIBS
-    OUTPUT_VARIABLE QT_INSTALL_LIBS
+    OUTPUT_VARIABLE _QT_INSTALL_LIBS
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 # find core library
-find_path(QTAV_INCLUDE_DIR NAMES QtAV.h
-    HINTS ${QT_INSTALL_HEADERS}
+find_path(QTAV_INCLUDE_DIR
+    NAMES QtAV.h
+    HINTS ${_QT_INSTALL_HEADERS}
     PATH_SUFFIXES QtAV
 )
-find_library(QTAV_LIBRARY NAMES QtAV QtAV1
-    HINTS ${QT_INSTALL_LIBS}
+find_library(QTAV_LIBRARY
+    NAMES QtAV QtAV1
+    HINTS ${_QT_INSTALL_LIBS}
 )
 
-# set output variables
-set(QtAV_INCLUDE_DIRS ${QTAV_INCLUDE_DIR})
-set(QtAV_LIBRARIES ${QTAV_LIBRARY})
-mark_as_advanced(QtAV_INCLUDE_DIRS QtAV_LIBRARIES)
-
-# create imported target for QtAV
-add_library(QtAV::QtAV UNKNOWN IMPORTED)
-set_property(TARGET QtAV::QtAV PROPERTY IMPORTED_LOCATION  ${QtAV_LIBRARIES})
-target_include_directories(QtAV::QtAV INTERFACE ${QtAV_INCLUDE_DIRS})
-
-# do the same for QtAVWidgets, if requested
+# find widgets library, only if component is requested
 if ("Widgets" IN_LIST QtAV_FIND_COMPONENTS)
-    # find paths
-    find_path(QTAVWIDGETS_INCLUDE_DIR NAMES QtAVWidgets.h
-        HINTS ${QT_INSTALL_HEADERS}
+    find_path(QTAVWIDGETS_INCLUDE_DIR
+        NAMES QtAVWidgets.h
+        HINTS ${_QT_INSTALL_HEADERS}
         PATH_SUFFIXES QtAVWidgets
     )
-    find_library(QTAVWIDGETS_LIBRARY NAMES QtAVWidgets QtAVWidgets1
-        HINTS ${QT_INSTALL_LIBS}
+    find_library(QTAVWIDGETS_LIBRARY
+        NAMES QtAVWidgets QtAVWidgets1
+        HINTS ${_QT_INSTALL_LIBS}
     )
-
-    # check if found
-    if ((NOT QTAVWIDGETS_INCLUDE_DIR MATCHES "QTAVWIDGETS_INCLUDE_DIR-NOTFOUND")
-        AND (NOT QTAVWIDGETS_LIBRARY MATCHES "QTAVWIDGETS_LIBRARY-NOTFOUND"))
-        # set output variables
-        set(QtAV_Widgets_FOUND TRUE)
-        set(QtAVWidgets_INCLUDE_DIRS ${QTAVWIDGETS_INCLUDE_DIR})
-        set(QtAVWidgets_LIBRARIES ${QTAVWIDGETS_LIBRARY})
-        mark_as_advanced(QtAVWidgets_INCLUDE_DIRS QtAVWidgets_LIBRARIES)
-
-        # create import target
-        add_library(QtAV::Widgets UNKNOWN IMPORTED)
-        set_property(TARGET QtAV::Widgets PROPERTY IMPORTED_LOCATION ${QtAVWidgets_LIBRARIES})
-        target_include_directories(QtAV::Widgets INTERFACE ${QtAVWidgets_INCLUDE_DIRS})
-        target_link_libraries(QtAV::Widgets INTERFACE QtAV::QtAV)
-
-        find_package_message(QtAVWidgets "Found QtAVWidgets: ${QtAVWidgets_LIBRARIES}"
-                             "[${QtAVWidgets_INCLUDE_DIRS}][${QtAVWidgets_LIBRARIES}]")
-    endif()
 endif()
 
-# check that the main library was found, and the Widgets component, if requested
+# unset temporary vars
+unset(_QT_INSTALL_HEADERS)
+unset(_QT_INSTALL_LIBS)
+
+if (QTAVWIDGETS_INCLUDE_DIR AND QTAVWIDGETS_LIBRARY)
+    # print a message manually for widgets. fphsa will print one for the main QtAV
+    find_package_message(QtAVWidgets "Found QtAVWidgets: ${QTAVWIDGETS_LIBRARY}"
+                         "[${QTAVWIDGETS_INCLUDE_DIR}][${QTAVWIDGETS_LIBRARY}]")
+    set(QtAV_Widgets_FOUND TRUE)
+endif()
+
+
+# fail if required things weren't found, and set QtAV_FOUND and other variables.
+# depends on component found variables (QtAV_Widgets_FOUND) already being set
 find_package_handle_standard_args(QtAV
     REQUIRED_VARS QTAV_LIBRARY QTAV_INCLUDE_DIR
     HANDLE_COMPONENTS
 )
+
+# Mark these cache variables as advanced. Before CMP0102 (cmake 3.17), this can only be
+# done if they've been already set
+if (QtAV_FOUND)
+    mark_as_advanced(QTAV_INCLUDE_DIR QTAV_LIBRARY)
+endif()
+if (QtAV_Widgets_FOUND)
+    mark_as_advanced(QTAVWIDGETS_INCLUDE_DIR QTAVWIDGETS_LIBRARY)
+endif()
+
+# create imported library targets
+if (QtAV_FOUND AND NOT TARGET QtAV::QtAV)
+    add_library(QtAV::QtAV UNKNOWN IMPORTED)
+    set_property(TARGET QtAV::QtAV PROPERTY IMPORTED_LOCATION ${QTAV_LIBRARY})
+    target_include_directories(QtAV::QtAV INTERFACE ${QTAV_INCLUDE_DIR})
+endif()
+
+if (QtAV_Widgets_FOUND AND NOT TARGET QtAV::Widgets)
+    add_library(QtAV::Widgets UNKNOWN IMPORTED)
+    set_property(TARGET QtAV::Widgets PROPERTY IMPORTED_LOCATION ${QTAVWIDGETS_LIBRARY})
+    target_include_directories(QtAV::Widgets INTERFACE ${QTAVWIDGETS_INCLUDE_DIR})
+    target_link_libraries(QtAV::Widgets INTERFACE QtAV::QtAV)
+endif()
